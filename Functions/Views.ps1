c8c3b54aef293295e29663bacb994f8f373f9076
thewerthon <eeeeewerton@gmail.com>
2025-05-29 16:56:16 -0300

    Work in progress

--- Functions/Views.ps1
+++ Functions/Views.ps1
@@ -114,15 +114,53 @@ Function Set-View {
 
                 }
 
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Documentos") -And $View.ServerRelativeUrl.EndsWith("Recentes.aspx")) { }
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Documentos") -And $View.ServerRelativeUrl.EndsWith("Pastas.aspx")) { }
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Documentos") -And $View.ServerRelativeUrl.EndsWith("Lista.aspx")) { }
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Documentos") -And $View.ServerRelativeUrl.EndsWith("my-sub.aspx")) { }
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Documentos") -And $View.ServerRelativeUrl.EndsWith("mod-view.aspx")) { }
+                If ($View.ServerRelativeUrl.EndsWith("/Documentos/Atuais/Forms/Pastas.aspx")) {
+                    
+                    $ViewFields = @('DocIcon', 'LinkFilename', 'ElaboradoPor', 'ElaboradoEm', 'VerificadoPor', 'VerificadoEm', 'AprovadoPor', 'AprovadoEm', '_ModerationStatus', 'Validade', '_DisplayName', '_ComplianceTag', 'FileSizeDisplay')
+                    $ViewWidths = '<FieldRef Name="Nome" width="500" /><FieldRef Name="Área" width="250" /><FieldRef Name="Categoria" width="250" /><FieldRef Name="Nível" width="120" /><FieldRef Name="Origem" width="120" /><FieldRef Name="Elaborado Por" width="180" /><FieldRef Name="Elaborado Em" width="130" /><FieldRef Name="Verificado Por" width="180" /><FieldRef Name="Verificado Em" width="130" /><FieldRef Name="Aprovado Por" width="180" /><FieldRef Name="Aprovado Em" width="130" /><FieldRef Name="Estado" width="120" /><FieldRef Name="Válido Até" width="120" /><FieldRef Name="Retenção" width="160" /><FieldRef Name="Confidencialidade" width="160" /><FieldRef Name="Tamanho do Arquivo" width="100" />'
+                    Set-PnPView -Identity $View.Id -List $View.ParentList.Id -Values @{ ColumnWidth = $ViewWidths } -Fields $ViewFields -Connection $Connection | Out-Null
 
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Registros") -And $View.ServerRelativeUrl.EndsWith("Recentes.aspx")) { }
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Registros") -And $View.ServerRelativeUrl.EndsWith("Pastas.aspx")) { }
-                If ($View.ParentList.ParentSite.Url.EndsWith("/Registros") -And $View.ServerRelativeUrl.EndsWith("Lista.aspx")) { }
+                }
+
+                If ($View.ServerRelativeUrl.EndsWith("/Documentos/Atuais/Forms/Lista.aspx")) {
+                    
+                    $ViewFields = @('DocIcon', 'LinkFilename', 'AreaDocumento', 'CategoriaDocumento', 'Categoria_x003a_N_x00ed_vel', 'Origem', 'ElaboradoPor', 'ElaboradoEm', 'VerificadoPor', 'VerificadoEm', 'AprovadoPor', 'AprovadoEm', '_ModerationStatus', 'Validade', '_DisplayName', '_ComplianceTag', 'FileSizeDisplay')
+                    $ViewWidths = '<FieldRef Name="Nome" width="500" /><FieldRef Name="Área" width="250" /><FieldRef Name="Categoria" width="250" /><FieldRef Name="Nível" width="120" /><FieldRef Name="Origem" width="120" /><FieldRef Name="Elaborado Por" width="180" /><FieldRef Name="Elaborado Em" width="130" /><FieldRef Name="Verificado Por" width="180" /><FieldRef Name="Verificado Em" width="130" /><FieldRef Name="Aprovado Por" width="180" /><FieldRef Name="Aprovado Em" width="130" /><FieldRef Name="Estado" width="120" /><FieldRef Name="Válido Até" width="120" /><FieldRef Name="Retenção" width="160" /><FieldRef Name="Confidencialidade" width="160" /><FieldRef Name="Tamanho do Arquivo" width="100" />'
+                    Set-PnPView -Identity $View.Id -List $View.ParentList.Id -Values @{ ColumnWidth = $ViewWidths } -Fields $ViewFields -Connection $Connection | Out-Null
+
+                }
+
+                If ($View.ServerRelativeUrl.EndsWith("/Documentos/Obsoletos/Forms/Pastas.aspx")) {
+
+                    $ViewFields = @('DocIcon', 'LinkFilename', 'ElaboradoPor', 'ElaboradoEm', 'VerificadoPor', 'VerificadoEm', 'AprovadoPor', 'AprovadoEm', '_DisplayName', '_ComplianceTag', 'FileSizeDisplay')
+                    $ViewWidths = '<FieldRef Name="Nome" width="500" /><FieldRef Name="Área" width="250" /><FieldRef Name="Categoria" width="250" /><FieldRef Name="Nível" width="120" /><FieldRef Name="Origem" width="120" /><FieldRef Name="Elaborado Por" width="180" /><FieldRef Name="Elaborado Em" width="130" /><FieldRef Name="Verificado Por" width="180" /><FieldRef Name="Verificado Em" width="130" /><FieldRef Name="Aprovado Por" width="180" /><FieldRef Name="Aprovado Em" width="130" /><FieldRef Name="Retenção" width="160" /><FieldRef Name="Confidencialidade" width="160" /><FieldRef Name="Tamanho do Arquivo" width="100" />'
+                    Set-PnPView -Identity $View.Id -List $View.ParentList.Id -Values @{ ColumnWidth = $ViewWidths } -Fields $ViewFields -Connection $Connection | Out-Null
+                    
+                }
+
+                If ($View.ServerRelativeUrl.EndsWith("/Documentos/Obsoletos/Forms/Lista.aspx")) {
+                    
+                    $ViewFields = @('DocIcon', 'LinkFilename', 'AreaDocumento', 'CategoriaDocumento', 'Categoria_x003a_N_x00ed_vel', 'Origem', 'ElaboradoPor', 'ElaboradoEm', 'VerificadoPor', 'VerificadoEm', 'AprovadoPor', 'AprovadoEm', '_DisplayName', '_ComplianceTag', 'FileSizeDisplay')
+                    $ViewWidths = '<FieldRef Name="Nome" width="500" /><FieldRef Name="Área" width="250" /><FieldRef Name="Categoria" width="250" /><FieldRef Name="Nível" width="120" /><FieldRef Name="Origem" width="120" /><FieldRef Name="Elaborado Por" width="180" /><FieldRef Name="Elaborado Em" width="130" /><FieldRef Name="Verificado Por" width="180" /><FieldRef Name="Verificado Em" width="130" /><FieldRef Name="Aprovado Por" width="180" /><FieldRef Name="Aprovado Em" width="130" /><FieldRef Name="Retenção" width="160" /><FieldRef Name="Confidencialidade" width="160" /><FieldRef Name="Tamanho do Arquivo" width="100" />'
+                    Set-PnPView -Identity $View.Id -List $View.ParentList.Id -Values @{ ColumnWidth = $ViewWidths } -Fields $ViewFields -Connection $Connection | Out-Null
+
+                }
+
+                If ($View.ServerRelativeUrl.EndsWith("/Registros/Atuais/Forms/AllItems.aspx")) {
+
+                    $ViewFields = @('DocIcon', 'LinkFilename', 'Author', 'Created', 'Editor', 'Modified', '_DisplayName', '_ComplianceTag', 'FileSizeDisplay')
+                    $ViewWidths = '<FieldRef Name="Nome" width="500" /><FieldRef Name="Criado Por" width="200" /><FieldRef Name="Criado Em" width="200" /><FieldRef Name="Modificado Por" width="200" /><FieldRef Name="Modificado Em" width="200" /><FieldRef Name="Retenção" width="160" /><FieldRef Name="Confidencialidade" width="160" /><FieldRef Name="Tamanho do Arquivo" width="100" />'
+                    Set-PnPView -Identity $View.Id -List $View.ParentList.Id -Values @{ ColumnWidth = $ViewWidths } -Fields $ViewFields -Connection $Connection | Out-Null
+
+                }
+
+                If ($View.ServerRelativeUrl.EndsWith("/Registros/Obsoletos/Forms/AllItems.aspx")) {
+
+                    $ViewFields = @('DocIcon', 'LinkFilename', 'Author', 'Created', 'Editor', 'Modified', '_DisplayName', '_ComplianceTag', 'FileSizeDisplay')
+                    $ViewWidths = '<FieldRef Name="Nome" width="500" /><FieldRef Name="Criado Por" width="200" /><FieldRef Name="Criado Em" width="200" /><FieldRef Name="Modificado Por" width="200" /><FieldRef Name="Modificado Em" width="200" /><FieldRef Name="Retenção" width="160" /><FieldRef Name="Confidencialidade" width="160" /><FieldRef Name="Tamanho do Arquivo" width="100" />'
+                    Set-PnPView -Identity $View.Id -List $View.ParentList.Id -Values @{ ColumnWidth = $ViewWidths } -Fields $ViewFields -Connection $Connection | Out-Null
+
+                }
 
             }
 
